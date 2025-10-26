fce734acaeafaa112165def7ad3dbd61001ec5e1
ronAa21 <aaronpagente19@gmail.com>
2025-10-26 20:24:33 +0800

    faq privacy forms

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+    session_start([
+       'cookie_httponly' => true,
+       'cookie_secure' => isset($_SERVER['HTTPS']),
+       'use_strict_mode' => true
+    ]);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+</body>
+</html>
